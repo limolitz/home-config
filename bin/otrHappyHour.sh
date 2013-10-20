999c298d47c3bd00470e973808967fbee83b7c8b
Florin Godard <fgodard@gmail.com>
2013-10-20 19:15:27 +0200

    Remove links on successfull download

--- bin/otrHappyHour.sh
+++ bin/otrHappyHour.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
-cat /home/florin/scripts/otr_happyHour.txt | while read line; do
+dataFile="/home/florin/bin/otr_happyHour.txt"
+tmpFile="/home/florin/bin/otr_happyHour_new.txt"
+
+cat $dataFile | while read line; do
    /bin/echo "Downloading $line"
-   /usr/bin/aria2c $line -d /media/hdd/aria2 -m 0 --retry-wait=30 --auto-file-renaming=false --on-download-complete=
+   /usr/bin/aria2c $line -d /tmp -m 0 --retry-wait=30 --auto-file-renaming=false --on-download-complete= >/dev/null
+   download=$?
+   case $download in
+   	0)
+				echo "$line downloaded."
+				grep -v "$line" $dataFile > $tmpFile
+				/bin/rm $dataFile
+				mv $tmpFile $dataFile
+				;;
+   	1)
+        echo "Error: unknown error"
+        ;;
+    2)
+     		;;
+    3)
+
+        ;;
+    13)
+				echo "File already exists."
+				;;
+    18)
+     		echo "Error: aria2 could not create directory."
+     		;;
+    *)
+        echo "Error: $line not downloaded. Unhandled status $download"
+        ;;
+    esac
 done
+
 #/bin/rm /home/florin/scripts/otr_happyHour.txt
 
