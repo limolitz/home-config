f711fd966cf5d3bcecf9cab98cb94c9a5deb7db1
Florin Godard <fgodard@gmail.com>
2013-10-26 01:09:35 +0200

    New handling for links

--- bin/otrHappyHour.sh
+++ bin/otrHappyHour.sh
@@ -2,39 +2,35 @@
 dataFile="/home/florin/bin/otr_happyHour.txt"
 tmpFile="/home/florin/bin/otr_happyHour_new.txt"
 
+echo -n "" > $tmpFile
+
 cat $dataFile | while read line; do
    /bin/echo "Downloading $line"
    /usr/bin/aria2c $line -d /media/hdd/aria2 -m 0 --retry-wait=30 --auto-file-renaming=false --on-download-complete= >/dev/null
    download=$?
+   retry=false
    case $download in
    	0)
 				echo "$line downloaded."
-				grep -v "$line" $dataFile > $tmpFile
-				/bin/rm $dataFile
-				mv $tmpFile $dataFile
 				;;
-   	1)
-        echo "Error: unknown error"
-        ;;
-    2)
-     		;;
-    3)
-
-        ;;
     13)
 				echo "File already exists."
-				grep -v "$line" $dataFile > $tmpFile
-                                /bin/rm $dataFile
-                                mv $tmpFile $dataFile
 				;;
     18)
      		echo "Error: aria2 could not create directory."
+        retry=true
      		;;
     *)
         echo "Error: $line not downloaded. Unhandled status $download"
+        retry=true
         ;;
     esac
+    # if download file, add link again for next run
+    if $retry ; then
+      echo "Retrying $line"
+      echo $line >> $tmpFile
+    fi
 done
 
-#/bin/rm /home/florin/scripts/otr_happyHour.txt
-
+/bin/rm $dataFile
+mv $tmpFile $dataFile
